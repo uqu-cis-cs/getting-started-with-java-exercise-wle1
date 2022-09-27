5fc327435d66d0fc21646ede4983c7ca1d314d82
Ali Alsaidi <waleed.w515w@gmail.com>
2022-09-27 18:24:43 +0300

    write a message

--- src/main/java/edu/uqu/cs/App.java
+++ src/main/java/edu/uqu/cs/App.java
@@ -11,10 +11,10 @@ package edu.uqu.cs;
  *      b.	Change <Student ID> to your student ID
  */
  
-class FirstExercise
-{
-    private static void mainMethod( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App
+{ 
+    public static void main( String[] args ) 
+     {        
+        System.out.println( "Hello : Waleed, 442004400" );
     }
 }
